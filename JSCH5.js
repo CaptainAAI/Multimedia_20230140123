f687e6c320ed90a1520aa603a6bf1ca4576e0732
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-04-28 11:50:36 +0700

    Add files via upload
    ok

---
+++ JSCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener('click', function() {
+    alert('OTW GEDUNG');
+});
+
+audio.addEventListener('click', function() {
+    alert('CARAMELLDANSEN');
+});
+
+gambar.addEventListener('click', function() {
+    alert('LOWI 280120Z AUTO VRB01KT 9999 NCD 09/07 Q1024');
+});
